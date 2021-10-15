4d66077ccb5b28a06b64782b623a130f991331aa
MagaliCorti <92572856+MagaliCorti@users.noreply.github.com>
2021-10-15 10:59:31 +0200

    Create R_first_code.r

---
+++ R_first_code.r
@@ -0,0 +1,14 @@
+# this is my first code in GitHub
+
+# here are the input data
+# costanza's data on stream
+water <- c(100, 200, 300, 400, 500)
+water
+
+# marta's data on fishes genomes
+fishes <- c(10, 50, 60, 100, 200)
+fishes
+
+# plot the diversity of fishes (y) vs the amount of water (x)
+plot(water, fishes)
+
