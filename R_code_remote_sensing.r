a55875e7ca65f364fa2f8ef8c1e79c7fd7332d43
MagaliCorti <92572856+MagaliCorti@users.noreply.github.com>
2021-11-08 10:57:15 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -89,3 +89,59 @@ plot(l2011$B2_sre, col=clg)
 plot(l2011$B3_sre, col=clr)
 
 
+
+
+# 08/11/2021
+
+library(raster)
+setwd("/Users/magalicorti/Desktop/lab/")
+brick("p224r63_2011.grd")
+l2011 <- brick("p224r63_2011.grd")
+
+l2011
+plot(l2011)
+plot(l2011$B1_sre)
+# changing color
+clb <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+clg <- colorRampPalette(c("dark green", "green", "light green"))(100)
+# multiframe
+par(mfrow=c(1,2)) 
+plot(l2011$B2_sre, col=clg)
+plot(l2011$B1_sre, col=clb)
+# inverting n of rows and column
+par(mfrow=c(2,1)) 
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# plot first 4 bands with 2 rows and 2 column
+clr <- colorRampPalette(c("dark red", "red", "pink"))(100)
+clnir <- colorRampPalette(c("red", "orange", "yellow"))(100)
+par(mfrow = c(2,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+plot(l2011$B3_sre, col=clr)
+plot(l2011$B4_sre, col=clnir)
+
+# plot the bands all together
+# before close the window with dev.off
+dev.off()
+# this is how we see the forest
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") # natural colors
+# extend vision to NIR -> switching from band 1, 2, 3 to 2, 3, 4
+# leaf has high reflectance in green (that's why we se them green)
+# but a huge reflectance also in NIR (which we cannot see)
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") # false colors
+# vegetation red because we put NIR as first component
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+# thanks to this vision we can clearly see the different umiidity of forest
+plotRGB(l2011, r=2, g=3, b=4, stretch="Lin")
+# we can clearly see bare soil (forest cut to make space to agriculture)
+par(mfrow=c(2,2)) 
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+# useful to analyze different aspects of forest
+
+
+
