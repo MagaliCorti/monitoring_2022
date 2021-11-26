390f321ac73d3d6cd13ff38bde13db33dc5a2da9
MagaliCorti <92572856+MagaliCorti@users.noreply.github.com>
2021-11-26 09:48:38 +0100

    Create R_code_chemical_cycling.r

---
+++ R_code_chemical_cycling.r
@@ -0,0 +1,10 @@
+# 26/11/2021
+
+# R code for chemical cycle studies
+# time series of NO2 change in Europe during lockdown
+
+setwd("/Users/magalicorti/Desktop/lab/en/")
+library(raster)
+
+# raster function importing every single layer one after the other creating a single layer
+raster("EN_0001.png")
