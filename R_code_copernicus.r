80d8ab50a42a846aae0be34e1f579cf7a0d0a84e
MagaliCorti <92572856+MagaliCorti@users.noreply.github.com>
2021-12-15 17:54:51 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -6,3 +6,17 @@ library(ggplot2)
 library(gridExtra) 
 library (RStoolbox)
 library(ncdf4)
+
+# importing copernicus data
+snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
+snow20211214 # see characteristics
+# 212400000 pixels -> real resolution
+plot(snow20211214)
+
+
+
+
+
+
+
+
