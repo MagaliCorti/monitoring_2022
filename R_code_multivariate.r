a9559dfd2ee23c37da907bc032b18980d689a8dd
MagaliCorti <92572856+MagaliCorti@users.noreply.github.com>
2021-12-22 09:33:09 +0100

    Create R_code_multivariate.r

---
+++ R_code_multivariate.r
@@ -0,0 +1,11 @@
+# 22/12/2021
+
+install.packages("vegan")
+
+setwd("/Users/magalicorti/Desktop/lab/")
+
+library(raster)
+library(ggplot2) 
+library(gridExtra) 
+library (RStoolbox)
+library(ncdf4)
